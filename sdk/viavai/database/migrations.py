eb46ed4c5abf7e88b1c2e5e7b338a1223d64f912
Sau1707 <Sau1707@users.noreply.github.com>
2026-01-28 20:26:34 +0100

    Refactor project structure: remove unused files and add initial CLI and database setup

---
+++ sdk/viavai/database/migrations.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from alembic import command
+from alembic.config import Config
+
+
+CURRENT_FILE = Path(__file__).resolve()
+
+import sys
+import importlib.util
+from pathlib import Path
+
+MODELS_PATH = Path.cwd() / "src" / "models"
+
+for py_file in MODELS_PATH.glob("*.py"):
+    if py_file.name.startswith("__"):
+        continue
+
+    module_name = f"src.models.{py_file.stem}"
+
+    spec = importlib.util.spec_from_file_location(module_name, py_file)
+    if spec is None or spec.loader is None:
+        continue
+    module = importlib.util.module_from_spec(spec)
+    print(f"Importing module: {module_name} from {py_file}")
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+from viavai.database.base import Base, engine
+
+
+def make_migrations():
+    cfg = Config()
+    cfg.set_main_option("script_location", str(CURRENT_FILE.parent))
+    cfg.set_main_option("version_locations", "migrations")
+
+    command.revision(cfg)
+
+
+def migrate():
+    cfg = Config()
+    cfg.set_main_option("script_location", str(CURRENT_FILE.parent))
+    cfg.set_main_option("version_locations", "migrations")
+    command.upgrade(cfg, "head")
+
+
+
+if __name__ == "__main__":
+    make_migrations()
+    migrate()
