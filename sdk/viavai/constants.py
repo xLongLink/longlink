6bbab890a0544d8a615319a8cff2e7ac2ac8a022
Leonardo Saurwein <leonardo@saurwein.ch>
2026-01-29 11:03:11 +0100

    Save

---
+++ sdk/viavai/constants.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+# Path to the root of viavai SDK
+PATH = Path(__file__).resolve().parent
---
+++ sdk/viavai/sample/main.py
@@ -0,0 +1,8 @@
+from viavai import ViaVai
+
+
+app = ViaVai(
+    title="FastAPI Feature Demo",
+    description="Single-file example of common FastAPI features",
+    version="1.0.0"
+)
