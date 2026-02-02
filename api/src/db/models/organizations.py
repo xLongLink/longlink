6a01708855dffc9539c9ac37f6dc5f4d270dce3b
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-03 00:29:37 +0100

    Cleanup

--- api/src/db/models/organizations.py
+++ api/src/db/models/organizations.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from sqlalchemy import String, DateTime, BigInteger, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column
 from src.db.models.__base__ import Base
-from .users import User
 
 
 class Organization(Base):
@@ -14,24 +13,6 @@ class Organization(Base):
     
     # Date tracking
     date_creation: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
-
-    # legal_name = Column(String(200), nullable=True)
-    # legal_form = Column(String(100), nullable=True)
-    # business_description = Column(String(1000), nullable=True)
-
-    # incorporation_date
-    # jurisdiction_country
-    # jurisdiction_region   (state / canton / province)
-    # registration_number
-    # company_status        (active, dissolved, liquidation, bankruptcy)
-
-    # registered_address_id
-    # operating_address_id
-
-    # industry_code
-    # industry_code_system  (NACE / NAICS / NOGA / SIC)
-    # business_description
-    # fiscal_year_end
     
 
 class OrganizationMember(Base):
@@ -44,7 +25,10 @@ class OrganizationMember(Base):
     id_user: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id"), nullable=False)
     
     role = mapped_column(String(50), nullable=False)
-
+    
+    # Date tracking
+    date_creation: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
+    
 
 class OrganizationSetting(Base):
     pass
