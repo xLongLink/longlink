f638126d785349c38603e4a11a3a3548141e7146
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-03 17:39:09 +0100

    Add Organization and OrganizationMember models to the database initialization

--- api/src/db/models/__init__.py
+++ api/src/db/models/__init__.py
@@ -2,3 +2,5 @@ from .__base__ import Base
 
 # Import all models here to ensure they are registered with Base
 from .users import User
+from .organizations import Organization
+from .organizations import OrganizationMember
