1aeca04ee632dbf74f27f83cb3f83c08335b97ea
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-03 01:30:49 +0100

    Split class

---
+++ sdk/viavai/logs.py
@@ -0,0 +1,16 @@
+class Logs:
+    def info(self, message: str):
+        pass
+
+    def debug(self, message: str):
+        pass
+
+    def warning(self, message: str):
+        pass
+
+    def error(self, message: str):
+        pass
+
+    def critical(self, message: str):
+        pass
+    
