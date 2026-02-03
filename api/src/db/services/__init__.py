c69fc048c6bc72c5e430ab4c71f927a464ee21f8
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-03 17:16:56 +0100

    Add organization create and read endpoints

--- api/src/db/services/__init__.py
+++ api/src/db/services/__init__.py
@@ -2,3 +2,6 @@ from .users import get_user_by_id
 from .users import get_user_by_email
 from .users import get_user_by_github_id
 from .users import add_user
+from .orgs import add_org
+from .orgs import add_org_member
+from .orgs import get_org_by_id
