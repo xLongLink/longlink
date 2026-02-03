639c84f54cc54fbdd9edb90dfc60f9ec1be5e652
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-03 12:37:37 +0100

    Update

--- sdk/viavai/cli/init.py
+++ sdk/viavai/cli/init.py
@@ -1,4 +1,3 @@
-# Init command, shall create a new viavai project structure
 import os
 import click
 from pathlib import Path
@@ -9,24 +8,5 @@ def setup(folder: Path):
     """Initialize a new viavai project"""
 
     os.makedirs(folder, exist_ok=True)
-    os.makedirs(folder / 'migrations', exist_ok=True)
-
-    os.makedirs(folder / 'src', exist_ok=True)
-    os.makedirs(folder / 'src' / 'models', exist_ok=True)
-    os.makedirs(folder / 'src' / 'routes', exist_ok=True)
-    os.makedirs(folder / 'tests', exist_ok=True)
-    
-    # Create main.py from template  
-    with open(PATH / 'sample' / 'main.py', 'r') as src:
-        template_content = src.read()
-        with open(folder / 'main.py', 'w') as dst:
-            dst.write(template_content)
-
-    # Create test.py from template
-    with open(PATH / 'sample' / 'test.py', 'r') as src:
-        test_content = src.read()
-        with open(folder / 'tests' / 'test.py', 'w') as dst:
-            dst.write(test_content)
-            
     click.echo(f"Project initialized in {folder}")
 
