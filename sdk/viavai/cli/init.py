9c2169525caa867cae1d4ddcdf08675ac3df12e4
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-03 17:37:09 +0100

    Copy sample scaffold on init

--- sdk/viavai/cli/init.py
+++ sdk/viavai/cli/init.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import click
 from pathlib import Path
 from viavai.constants import PATH
@@ -6,7 +7,7 @@ from viavai.constants import PATH
 
 def setup(folder: Path):
     """Initialize a new viavai project"""
-
+    sample_path = PATH / "sample"
     os.makedirs(folder, exist_ok=True)
+    shutil.copytree(sample_path, folder, dirs_exist_ok=True)
     click.echo(f"Project initialized in {folder}")
-
