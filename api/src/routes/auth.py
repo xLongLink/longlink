8dfea18f3b76959b5b886f5c5db9e455a288bda9
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-05 14:04:21 +0100

    Refactor db access namespaces

--- api/src/routes/auth.py
+++ api/src/routes/auth.py
@@ -1,5 +1,5 @@
 from typing import cast
-from src.db import add_user
+import src.db as db
 from fastapi import Request, HTTPException
 from src.auth import oauth
 from src.router import router
@@ -7,46 +7,46 @@ from fastapi.responses import RedirectResponse
 from authlib.integrations.starlette_client.apps import StarletteOAuth2App
 
 
-@router.get("/login/github")
+@router.get('/login/github')
 async def login_github(request: Request):
-    github = cast(StarletteOAuth2App, oauth.create_client("github"))
+    github = cast(StarletteOAuth2App, oauth.create_client('github'))
 
     return await github.authorize_redirect(
         request,
-        redirect_uri="http://localhost:8000/auth/github"
+        redirect_uri='http://localhost:8000/auth/github',
     )
 
 
-@router.get("/auth/github")
+@router.get('/auth/github')
 async def auth_github(request: Request):
-    github = cast(StarletteOAuth2App, oauth.create_client("github"))
+    github = cast(StarletteOAuth2App, oauth.create_client('github'))
 
     token = await github.authorize_access_token(request)
-    userinfo = await github.get("user", token=token)
+    userinfo = await github.get('user', token=token)
 
     # Ensure that the user exists in our database
-    user = await add_user(
-        name=userinfo.json().get("name") or userinfo.json().get("login"),
-        email=userinfo.json().get("email") or f"sample@localhost",
-        avatar=userinfo.json().get("avatar_url"),
-        oauth_github_id=userinfo.json().get("id"),
+    user = await db.users.create(
+        name=userinfo.json().get('name') or userinfo.json().get('login'),
+        email=userinfo.json().get('email') or 'sample@localhost',
+        avatar=userinfo.json().get('avatar_url'),
+        oauth_github_id=userinfo.json().get('id'),
     )
 
-    request.session["userid"] = user.id
-    return RedirectResponse("/")
+    request.session['userid'] = user.id
+    return RedirectResponse('/')
 
 
 
-@router.get("/logout")
+@router.get('/logout')
 async def logout(request: Request):
     request.session.clear()
-    return {"ok": True}
+    return {'ok': True}
     
 
 
-@router.get("/me")
+@router.get('/me')
 async def me(request: Request):
-    user = request.session.get("user")
+    user = request.session.get('user')
     if not user:
-        raise HTTPException(401, "Not authenticated")
+        raise HTTPException(401, 'Not authenticated')
     return user
