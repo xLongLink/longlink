3495a74b17ba529c93f377006a0b8b370f2d0a8e
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-05 14:40:40 +0100

    Add apps service to db layer

--- api/src/db/models/__init__.py
+++ api/src/db/models/__init__.py
@@ -1,6 +1,6 @@
-from .__base__ import Base
-
 # Import all models here to ensure they are registered with Base
+from .apps import App
 from .users import User
+from .__base__ import Base
 from .organizations import Organization
 from .organizations import OrganizationMember
--- api/src/routes/org.py
+++ api/src/routes/org.py
@@ -1,14 +1,15 @@
 import src.db as db
-from fastapi import Depends, HTTPException
+from src.router import router
 from src.auth import user as get_user
 from src.types import OrgCreate, OrgRead
-from src.router import router
+from fastapi import Depends, HTTPException
 
 
 @router.post('/org', response_model=OrgRead)
 async def create_org(payload: OrgCreate, current_user: db.User = Depends(get_user)):
     org = await db.orgs.create(payload.name)
     await db.orgs.add(org.id, current_user.id, 'owner')
+    await db.apps.create(org.id, payload.name)
     return OrgRead(
         id=org.id,
         name=org.name,
