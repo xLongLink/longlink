e5b1ec2a47a4b6d2007d0e3ea639c32773659a1f
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-02-05 16:35:28 +0100

    Rename organization models

--- api/src/db/services/orgs.py
+++ api/src/db/services/orgs.py
@@ -1,26 +1,26 @@
 from sqlalchemy import select
-from src.db.models import Organization, OrganizationMember
 from src.db.session import get_session
+from src.db.models import Org, OrgMember
 
 
 class OrgsService:
-    async def create(self, name: str) -> Organization:
+    async def create(self, name: str) -> Org:
         """Add a new organization to the database."""
 
         Session = await get_session()
         async with Session() as session:
-            org = Organization(name=name)
+            org = Org(name=name)
             session.add(org)
             await session.commit()
             await session.refresh(org)
             return org
 
-    async def add(self, org_id: int, user_id: int, role: str) -> OrganizationMember:
+    async def add(self, org_id: int, user_id: int, role: str) -> OrgMember:
         """Add a new member to an organization."""
 
         Session = await get_session()
         async with Session() as session:
-            member = OrganizationMember(
+            member = OrgMember(
                 id_org=org_id,
                 id_user=user_id,
                 role=role,
@@ -30,22 +30,22 @@ class OrgsService:
             await session.refresh(member)
             return member
 
-    async def get(self, org_id: int) -> Organization | None:
+    async def get(self, org_id: int) -> Org | None:
         """Retrieve an organization by its ID."""
 
         Session = await get_session()
         async with Session() as session:
-            result = await session.execute(select(Organization).where(Organization.id == org_id))
+            result = await session.execute(select(Org).where(Org.id == org_id))
             return result.scalars().first()
 
-    async def list_by_user(self, user_id: int) -> list[Organization]:
+    async def list_by_user(self, user_id: int) -> list[Org]:
         """Retrieve organizations assigned to a user."""
 
         Session = await get_session()
         async with Session() as session:
             result = await session.execute(
-                select(Organization)
-                .join(OrganizationMember, Organization.id == OrganizationMember.id_org)
-                .where(OrganizationMember.id_user == user_id)
+                select(Org)
+                .join(OrgMember, Org.id == OrgMember.id_org)
+                .where(OrgMember.id_user == user_id)
             )
             return list(result.scalars().all())
