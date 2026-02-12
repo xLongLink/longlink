ecc4bd418a07508aa1b0bb56a7ec7600a19b6a0e
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-12 15:44:57 +0100

    feat: create Form component and add Field Components documentation

---
+++ web/src/components/viavai/form.tsx
@@ -0,0 +1,25 @@
+// Create a Form component that is schema-driven system for defining and managing forms in React, so that 
+
+// types:
+// - text
+// - password
+// - textarea
+// - number
+// - date
+// - select
+// - radio
+// - checkbox
+// - checkbox-group
+// - switch
+// - tags 
+// - upload
+// eventually more
+
+
+// Params: 
+// - schema={schema}
+// - onSubmit = {(data) => console.log(data)}
+// - submitLabel = "Save"
+export default function Form() {
+    return <></>
+}
