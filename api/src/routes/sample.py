3985626f20387e901505443b84640a65f3d7aa3c
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-14 15:44:04 +0100

    fix: update endpoint to return page schema instead of form schema

--- api/src/routes/sample.py
+++ api/src/routes/sample.py
@@ -4,10 +4,10 @@ from src.router import router
 
 # A LongLink page takes a URL, with filders and stuff. If is not possible to show in a single page then the content is too complex
 # Each POST request from the page shall automatically re-render the page. Given that we assume new data has come
-@router.get('/sample/form', response_model=list[Component])
+@router.get('/sample/page', response_model=list[Component])
 async def form():
     """Return an example page schema payload with a hero component."""
     page = Page()
-    hero = page.hero(title="Data Table")
+    page.hero(title="Data Table")
 
     return list(page)
