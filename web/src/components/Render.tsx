3f1d7f174ef17f2001829454bceff475a2c5907f
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-02-15 22:21:51 +0100

    web: merge Column into Columns component

--- web/src/components/Render.tsx
+++ web/src/components/Render.tsx
@@ -1,6 +1,5 @@
 import type React from 'react';
 import Button from '@/components/viavai/Button';
-import Column from '@/components/viavai/Column';
 import Columns from '@/components/viavai/Columns';
 import Dialog from '@/components/viavai/Dialog';
 import Hero from '@/components/viavai/Hero';
@@ -15,7 +14,6 @@ const registry = {
     button: Button,
     table: Table,
     columns: Columns,
-    column: Column,
     separator: Separator,
 };
 
