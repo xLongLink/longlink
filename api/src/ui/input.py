4f0e3bff1c8c183ca6b78bf580067c20a22a37aa
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-16 16:16:51 +0100

    feat: add Input component for generic input handling

---
+++ api/src/ui/input.py
@@ -0,0 +1,24 @@
+from typing import Literal, TypeAlias
+from dataclasses import dataclass, field
+from src.ui.__root__ import Component
+
+
+# Generic input. This can be extended to support specific types of input, like text, number, select, etc. For now, it's just a placeholder.
+
+
+@dataclass
+class Input(Component):
+    label: str | None = None
+    placeholder: str | None = None
+    description: str | None = None
+    submit: str | None = None # Text of the submit button, if any
+
+    def __iter__(self):
+        yield 'type', 'input'
+        yield 'props', {
+            'label': self.label,
+            'placeholder': self.placeholder,
+            'description': self.description,
+            'submit': self.submit,
+        }
+        yield 'children', []
