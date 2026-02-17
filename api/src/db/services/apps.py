f8e5e1eb765146fa2d1a6e04abc472f8e181e28f
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-02-17 20:50:10 +0100

    Require app URL when creating apps

--- api/src/db/services/apps.py
+++ api/src/db/services/apps.py
@@ -3,12 +3,12 @@ from src.db.session import get_session
 
 
 class AppsService:
-    async def create(self, name: str) -> App:
+    async def create(self, name: str, url: str) -> App:
         """Add a new app to the database."""
 
         Session = await get_session()
         async with Session() as session:
-            app = App(name=name)
+            app = App(name=name, url=url)
             session.add(app)
             await session.commit()
             await session.refresh(app)
