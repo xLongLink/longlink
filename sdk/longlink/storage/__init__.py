7ea1d9ab83850644005209d1b2652fd53072af4c
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-17 16:37:07 +0100

    refactor: update storage class documentation and structure

--- sdk/longlink/storage/__init__.py
+++ sdk/longlink/storage/__init__.py
@@ -1,5 +1,10 @@
 # TODO: Storage
 
+# On the API it enables the connections / permissions / ecc
+# here a folder is created for each applications
+# Google Drive (via Google Workspace)
+# OneDrive (via Microsoft 365)
+# Normal SFTP folder
 
 class Storage:
     pass
