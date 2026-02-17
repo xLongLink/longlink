9ab8af86d4b81c659a19a08ae22f044dda39ecc6
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-17 18:05:46 +0100

    refactor: update settings class structure and improve organization-specific settings

--- api/src/settings.py
+++ api/src/settings.py
@@ -2,14 +2,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str
-    debug: bool = False
-    database_url: str
+    """App settings, those are all ENV"""
 
+    # Organization specitic settings that can be re-used in the tools
+    ORG_NAME: str
+    # ORG_COUNTRY: str
+    # ORG_ID: str
+    # ORG_NAME: str
+    # ORG_SECTOR: str
+    
+    DATABASE_URL: str
+
+    GITHUB_CLIENT_ID: str | None = None
+    GITHUB_CLIENT_SECRET: str | None = None
+    
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
     )
 
 
-settings = Settings() # type: ignore
+settings = Settings()
