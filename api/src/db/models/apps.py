7b108cb775c9803d8778cc088d1ca3b913031e45
Leonardo Saurwein <leonardo@saurwein.ch>
2026-02-20 15:44:54 +0100

    Refactor database models to standardize column names and add timestamp fields

--- api/src/db/models/apps.py
+++ api/src/db/models/apps.py
@@ -6,12 +6,13 @@ from src.db.models.__base__ import Base
 
 class App(Base):
     """Represent an application installed in the platform."""
-
     __tablename__ = 'apps'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
     url: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
-    token_hash: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    token: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
 
+    # Timestamp informations
+    date_update: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
     date_creation: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
