7f40c33dbc4269f19d68e3022d6e882a35dae681
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-20 14:49:08 +0100

    Add app-specific env secrets database layer

--- api/src/db/models/__init__.py
+++ api/src/db/models/__init__.py
@@ -1,5 +1,6 @@
 # Import all models here to ensure they are registered with Base
 from .__base__ import Base
 from .apps import App
+from .envs import Env
 from .settings import Setting
 from .users import User
--- api/src/db/services/__init__.py
+++ api/src/db/services/__init__.py
@@ -1,3 +1,4 @@
 from .apps import AppsService
+from .envs import EnvsService
 from .settings import SettingsService
 from .users import UsersService
