63a74a9fabd3ddc83694f362c7071e1a345f4a51
Sau1707 <Sau1707@users.noreply.github.com>
2026-02-21 21:25:27 +0100

    Fix application entry point in dev command and clean up main.py

--- sdk/sample/main.py
+++ sdk/sample/main.py
@@ -1,7 +1,5 @@
 # TODO: Import everything here? 
 # TODO: Settings??
-
-
 from src.app import app
 
 
@@ -9,17 +7,4 @@ from src.app import app
 import src.routes.sample
 import src.pages.settings
 import src.pages.table
-
-
-if __name__ == '__main__':
-    import uvicorn
-    from dotenv import load_dotenv
-    load_dotenv()
-
-    uvicorn.run(
-        'main:app',
-        host='0.0.0.0',
-        port=1707,
-        reload=True
-    )
 
