98d2d96f60b851126b08701269aa151ea1e389f7
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-03-01 19:09:28 +0100

    Rename general settings to organization and remove card wrapper

--- web/src/pages/org/Settings.tsx
+++ web/src/pages/org/Settings.tsx
@@ -25,7 +25,7 @@ export default function SettingsPage() {
                 <MenuList>
                     <MenuSection
                         value="general"
-                        label="General"
+                        label="Organization"
                         icon={GlobeIcon}
                     />
                     <MenuSection
