6b93f7a522413df19c9a3c0ca953bdf61c009cdf
Leonardo Saurwein <leonardo@saurwein.ch>
2026-03-01 18:18:37 +0100

    Refactor settings page to use dedicated General component and simplify menu structure

---
+++ web/src/components/settings/General.tsx
@@ -0,0 +1,6 @@
+import Hero from '@/components/longlink/Hero';
+
+
+export default function General() {
+    return <Hero title="General Settings" subtitle="Configure general organization settings" icon="settings" />;
+}
