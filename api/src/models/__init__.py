afbb2bd23e14873e60444e4202c8a2ec56770187
Sau1707 <Sau1707@users.noreply.github.com>
2026-03-01 17:34:08 +0100

    feat(api): add root database connection management endpoints

--- api/src/models/__init__.py
+++ api/src/models/__init__.py
@@ -1,4 +1,5 @@
 from .apps import AppCreate, AppResponse
+from .databases import DatabaseCreate, DatabaseResponse, DatabaseUpdate
 from .lists.countries import CountryCode
+from .settings import SettingResponse, SettingSet, SettingSetItem
 from .users import UserUpdate
-from .settings import SettingSet, SettingResponse, SettingSetItem
