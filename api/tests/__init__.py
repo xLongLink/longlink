8098f236d4b53a3fef5b077196b8b9de4fc3ede8
Leonardo Saurwein <leonardo@saurwein.ch>
2026-03-25 12:23:12 +0100

    Added sample testcontainers

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,1 +1,13 @@
 # Placeholder, do not implement anything for now
+
+
+
+"""
+import pytest
+from testcontainers.postgres import PostgresContainer
+
+@pytest.fixture
+def postgres():
+    with PostgresContainer("postgres:15") as pg:
+        yield pg
+"""
