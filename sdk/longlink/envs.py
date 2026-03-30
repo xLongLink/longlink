70e15e949402d0ec8d5a3eb82efb3ac9e69d7b18
Sau1707 <Sau1707@users.noreply.github.com>
2026-03-30 12:29:31 +0200

    Add development env defaults for longlink dev

--- sdk/longlink/envs.py
+++ sdk/longlink/envs.py
@@ -1,18 +1,21 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Envs(BaseSettings):
     """App specific secrets"""
+
     DEV: bool = False
 
     # LongLink internal
     # The application key, used to identify the application in the API
-    LLURL: str    # Example: "sample.longlink.ch"
-    LLKEY: str    # Example: "samplekey"
+    LLURL: str    # Example: 'sample.longlink.ch'
+    LLKEY: str    # Example: 'samplekey'
 
     # The application specific database
     DBURL: str
-    
+
     # The application specific storage
     storage_key: str
     storage_secret: str
@@ -22,4 +25,17 @@ class Envs(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
-envs = Envs() # type: ignore
+class EnvsDev(Envs):
+    DEV: bool = True
+    LLURL: str = 'http://localhost:8000'
+    LLKEY: str = 'longlink-dev'
+    DBURL: str = 'sqlite:///./longlink-dev.db'
+    storage_key: str = 'dev'
+    storage_secret: str = 'dev'
+    storage_endpoint: str = 'http://localhost:9000'
+
+
+if os.getenv('DEV', '').lower() in {'1', 'true', 'yes', 'on'}:
+    envs = EnvsDev()  # type: ignore
+else:
+    envs = Envs()  # type: ignore
