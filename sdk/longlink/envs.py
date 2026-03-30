4e387716c5314f5a9430060d1181608b77c461d7
Leonardo Saurwein <leonardo@saurwein.ch>
2026-03-30 12:19:36 +0200

    cleanup

--- sdk/longlink/envs.py
+++ sdk/longlink/envs.py
@@ -2,24 +2,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Envs(BaseSettings):
-    """App specific settings, extend this class to add more settings."""
-    dev: bool = False
+    """App specific secrets"""
+    DEV: bool = False
 
+    # LongLink internal
     # The application key, used to identify the application in the API
-    key: str
+    LLURL: str    # Example: "sample.longlink.ch"
+    LLKEY: str    # Example: "samplekey"
 
     # The application specific database
-    database_url: str
+    DBURL: str
     
     # The application specific storage
     storage_key: str
     storage_secret: str
     storage_endpoint: str
-    
-    model_config = SettingsConfigDict(
-        env_file=('.env.sample', '.env'),
-        env_file_encoding='utf-8'
-    )
+
+    # At runtime any changes to those values require a container restart.
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
 envs = Envs() # type: ignore
