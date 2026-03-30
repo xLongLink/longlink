5006dc7c0dd98ae4e98b607bb27c1540edaa0d92
Leonardo Saurwein <leonardo@saurwein.ch>
2026-03-30 23:25:35 +0200

    docs: update database adapter documentation for clarity and completeness

--- api/src/databases/__init__.py
+++ api/src/databases/__init__.py
@@ -1,16 +1,15 @@
 """
-This folder contains the database provider adapters
+This folder contains the database provider adapters. 
+If a superuser is added to the LongLink control plane, 
+when a new app is created, a new database will be created for that app.
+A database adapter is implemented for each database that support superuser access: 
+- PostgreSQL
+- TODO: MySQL Adapter
+- TODO: Oracle Adapter
+- TODO: PostgreSQL Adapter
+- TODO: MariaDB Adapter
 
-PostgreSQL (psycopg2)
-MySQL
-MariaDB
-
-Amazon Web Services → RDS / Aurora
-Google → Cloud SQL
-Microsoft → Azure Database
-Oracle → Oracle Cloud Database
-
-An app shall also be deployed with a direct database url string.
+An app can also be deployed with a direct database url string.
 """
 
 from src.databases.__root__ import Database
