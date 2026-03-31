74ae6165dbad34bb12845f097115c6656f29a573
Sau1707 <Sau1707@users.noreply.github.com>
2026-03-31 14:50:44 +0200

    api: add per-app user permissions table and service

--- api/src/db/__init__.py
+++ api/src/db/__init__.py
@@ -1,11 +1,12 @@
-from .models import App, Env, User, Setting, Storage, Database
+from .models import App, Env, User, Setting, Storage, Database, Permission
 from .services import (AppsService, EnvsService, UsersService, SettingsService,
-                       StoragesService, DatabasesService)
+                       StoragesService, DatabasesService, PermissionsService)
 
 apps = AppsService()
 databases = DatabasesService()
 envs = EnvsService()
 settings = SettingsService()
 users = UsersService()
+permissions = PermissionsService()
 
 storages = StoragesService()
--- api/src/db/services/__init__.py
+++ api/src/db/services/__init__.py
@@ -4,3 +4,4 @@ from .users import UsersService
 from .settings import SettingsService
 from .storages import StoragesService
 from .databases import DatabasesService
+from .permissions import PermissionsService
