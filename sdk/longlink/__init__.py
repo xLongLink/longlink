96afea80852b3ae74c8a5aaa9c429310704003a7
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-01 17:01:47 +0200

    Fix SDK internal route registration for /pages

--- sdk/longlink/__init__.py
+++ sdk/longlink/__init__.py
@@ -1,3 +1,5 @@
+# Import internal routes for side-effect registration on the global router.
+import longlink.routes  # noqa: E402,F401
 from longlink.ui import Page
 from longlink.app import create_app
 from longlink.router import get, put, page, post, pages, patch, route, delete
@@ -5,6 +7,3 @@ from longlink.organization import org
 
 # Official Starlette application instance.
 app = create_app()
-
-# Import internal routes for side-effect registration on the global router.
-import longlink.routes  # noqa: E402,F401
