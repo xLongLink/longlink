2f117559eb889a5980d63a1e1384fded819935c2
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-07 21:15:01 +0200

    Add database connection management and creation endpoints

--- api/src/db/__init__.py
+++ api/src/db/__init__.py
@@ -1,9 +1,11 @@
-from .models import App, Env, User, Setting, Permission
+from .models import App, Env, User, Setting, Permission, DatabaseConnection
 from .services import (AppsService, EnvsService, UsersService, SettingsService,
-                       PermissionsService)
+                       DatabasesService, PermissionsService)
 
 apps = AppsService()
 envs = EnvsService()
 settings = SettingsService()
 users = UsersService()
 permissions = PermissionsService()
+
+databases = DatabasesService()
