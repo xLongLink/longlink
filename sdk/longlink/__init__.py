be7857f02608662f85d48bc03b36cab66b5a1946
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-18 00:49:40 +0200

    feat(sdk): add request context dependency and aliases

--- sdk/longlink/__init__.py
+++ sdk/longlink/__init__.py
@@ -3,16 +3,23 @@
 from longlink.app import LongLink
 from longlink.envs import Settings, Enviroments, Environments
 from longlink.router import Router
+from longlink.context import (Context, ContextDep, SessionDep, StorageDep,
+                              get_context)
 from longlink.storage import Storage, get_storage
 from longlink.organization import org
 
 __all__ = [
+    "Context",
+    "ContextDep",
     "Environments",
     "Enviroments",
     "LongLink",
     "Router",
+    "SessionDep",
     "Settings",
     "Storage",
+    "StorageDep",
+    "get_context",
     "get_storage",
     "org",
 ]
