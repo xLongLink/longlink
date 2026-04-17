72356a99dd6f0e2a737df71a272b6d8afa052edf
Sau1707 <Sau1707@users.noreply.github.com>
2026-04-17 14:03:10 +0200

    sdk: switch sample routes to APIRouter-style registration

--- sdk/longlink/__init__.py
+++ sdk/longlink/__init__.py
@@ -4,6 +4,6 @@ from longlink.app import App, LongLink
 from longlink.envs import ENV, ENVDev, Envs, EnvsDev, envs, get_envs
 from longlink.organization import org
 from longlink.predefined import issues_page, sample_page
-from longlink.router import (delete, get, page, pages, patch, post, put, route,
-                             xml_page)
+from longlink.router import (LongLinkRouter, delete, get, page, pages, patch,
+                             post, put, route, xml_page)
 from longlink.xml import load_page_schema_from_xml
