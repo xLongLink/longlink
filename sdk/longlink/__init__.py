ea598acaed3528ec4f43fa12e6532a3ca785bcdf
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-17 12:07:45 +0200

    Refactor SDK app to use validated ENV wrapper

--- sdk/longlink/__init__.py
+++ sdk/longlink/__init__.py
@@ -1,11 +1,13 @@
 # Import internal routes for side-effect registration on the global router.
 import longlink.routes  # noqa: E402,F401
-from longlink.app import create_app
-from longlink.xml import load_page_schema_from_xml
-from longlink.router import (get, put, page, post, pages, patch, route, delete,
-                             xml_page)
-from longlink.predefined import issues_page, sample_page
+from longlink.app import App, create_app, create_longlink_app
+from longlink.envs import ENV, ENVDev, Envs, EnvsDev, envs, get_envs
 from longlink.organization import org
+from longlink.predefined import issues_page, sample_page
+from longlink.router import (delete, get, page, pages, patch, post, put, route,
+                             xml_page)
+from longlink.xml import load_page_schema_from_xml
 
-# Official Starlette application instance.
-app = create_app()
+# Official Starlette application wrapper and instance.
+application = create_longlink_app()
+app = application.fastapi
