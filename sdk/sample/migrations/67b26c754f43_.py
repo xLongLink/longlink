0f3a6c495e127c9763b6d7d2d887bdaa6e4d81f3
Sau1707 <leonardo@saurwein.ch>
2026-04-19 20:35:37 +0200

    Remove organization and permissions documentation; add initial migration file

---
+++ sdk/sample/migrations/67b26c754f43_.py
@@ -0,0 +1,20 @@
+"""
+Revision ID: 67b26c754f43
+Revises: 
+Create Date: 2026-04-19 20:34:30.871576
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = "67b26c754f43"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
