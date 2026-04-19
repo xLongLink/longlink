bb45908d334abbc6540e25a8f2b45b237750cf95
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-19 16:49:30 +0200

    sdk: re-export database Table in package init

--- sdk/longlink/__init__.py
+++ sdk/longlink/__init__.py
@@ -3,3 +3,4 @@ from longlink.app import LongLink
 from longlink.state import Context, get_context
 from longlink.utils import *
 from longlink.storage import Storage, create_storage
+from longlink.database import Base, Table, Database, get_session
