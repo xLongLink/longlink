6173e259312a8239f28fe8dbae343f31a1f7aa90
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-19 20:44:58 +0200

    api: remove legacy settings/storage/users endpoints and models

--- api/src/db/models/__init__.py
+++ api/src/db/models/__init__.py
@@ -3,7 +3,6 @@ from .apps import App
 from .envs import Env
 from .users import User
 from .__base__ import Base
-from .settings import Setting
 from .permissions import Permission
 
-__all__ = ["App", "Env", "User", "Base", "Setting", "Permission"]
+__all__ = ["App", "Env", "User", "Base", "Permission"]
--- api/src/routes/__init__.py
+++ api/src/routes/__init__.py
@@ -2,18 +2,12 @@ from src.routes.apps import router as apps_router
 from src.routes.auth import router as auth_router
 from src.routes.user import router as user_router
 from src.routes.pages import router as pages_router
-from src.routes.users import router as users_router
 from src.routes.proxies import router as proxies_router
-from src.routes.storage import router as storage_router
-from src.routes.settings import router as settings_router
 
 routers = [
     apps_router,
     auth_router,
     pages_router,
     proxies_router,
-    settings_router,
-    storage_router,
     user_router,
-    users_router,
 ]
